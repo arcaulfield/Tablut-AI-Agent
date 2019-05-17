d3863bed7ea3b1746ebfed36b8be4ae5378049c1
Michael Vaquier <michael.vaquier@mail.mcgill.ca>
2019-05-17 07:21:21 -0700

    Out of bounds bug updated in evaluaton function (#6)

--- src/tablut/TablutBoardState.java
+++ src/tablut/TablutBoardState.java
@@ -168,19 +168,19 @@ public class TablutBoardState extends BoardState {
     {
         List<Coord> neighbours = new ArrayList<>();
 
-        if(getPieceAt(position.x, position.y + 1) == colour)
+        if(position.y < BOARD_SIZE - 1 && getPieceAt(position.x, position.y + 1) == colour)
         {
             neighbours.add(Coordinates.get(position.x, position.y + 1));
         }
-        else if(getPieceAt(position.x + 1, position.y) == colour)
+        else if(position.x < BOARD_SIZE - 1 && getPieceAt(position.x + 1, position.y) == colour)
         {
             neighbours.add(Coordinates.get(position.x + 1, position.y));
         }
-        else if(getPieceAt(position.x - 1, position.y) == colour)
+        else if(position.x > 0 && getPieceAt(position.x - 1, position.y) == colour)
         {
             neighbours.add(Coordinates.get(position.x - 1, position.y));
         }
-        else if(getPieceAt(position.x, position.y - 1) == colour)
+        else if(position.y > 0 && getPieceAt(position.x, position.y - 1) == colour)
         {
             neighbours.add(Coordinates.get(position.x, position.y - 1));
         }
